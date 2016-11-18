85a2dfcde164602ad30b5975218e987163e4c276
jeremynapitupulu <jeremy150498@yahoo.com>
2016-11-18 20:33:33 +0700

    versi 1.0.0

---
+++ app/src/main/java/jeremynapitupulu/todolist/AboutDialog.java
@@ -0,0 +1,24 @@
+package jeremynapitupulu.todolist;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class AboutDialog extends DialogFragment {
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder theDialog = new AlertDialog.Builder(getActivity());
+
+        theDialog.setTitle("About");
+        theDialog.setMessage("To Do List v. 1.0.3 by JEREMY");
+        theDialog.setPositiveButton("OK", null);
+
+        /*return super.onCreateDialog(savedInstanceState);*/
+        return theDialog.create();
+    }
+}
